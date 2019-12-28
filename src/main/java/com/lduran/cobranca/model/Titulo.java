90e70189fab6b1c8b8510d103a45c55bd31e1997
Lindineu Lopes Duran <37485332+LindineuDuran@users.noreply.github.com>
2019-12-27 21:18:21 -0300

    Validações com Bean Validation

--- src/main/java/com/lduran/cobranca/model/Titulo.java
+++ src/main/java/com/lduran/cobranca/model/Titulo.java
@@ -4,6 +4,7 @@ import java.math.BigDecimal;
 import java.util.Date;
 
 import javax.persistence.*;
+import javax.validation.constraints.*;
 
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.format.annotation.NumberFormat;
@@ -21,14 +22,20 @@ public class Titulo
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long codigo;
 
+	@NotEmpty(message = "Descrição é obrigatória!")
+	@Size(max = 60, message = "A descrição não pode conter mais de 60 caracteres")
 	@EqualsAndHashCode.Exclude
 	private String descricao;
 
 	@EqualsAndHashCode.Exclude
+	@NotNull(message = "Data de Vencimento é obrigatória!")
 	@DateTimeFormat(pattern = "dd/MM/yyyy")
 	@Temporal(TemporalType.DATE)
 	private Date dataVencimento;
 
+	@NotNull(message = "Valor é obrigatório!")
+	@DecimalMin(value = "0.01", message = "Valor não pode ser menor que 0,01")
+	@DecimalMax(value = "9999999.99", message = "Valor não pode ser maior que 9.999.999,99")
 	@EqualsAndHashCode.Exclude
 	@NumberFormat(pattern = "#,##0.00")
 	private BigDecimal valor;
